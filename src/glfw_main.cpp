401110222740e8b9814c9af945fdfaf0db8d0d3a
jimbo00000 <james.susinno@gmail.com>
2015-03-07 21:42:47 -0500

    Draw rect to fill screen.

--- src/glfw_main.cpp
+++ src/glfw_main.cpp
@@ -155,19 +155,7 @@ int main(int argc, char** argv)
         glfwPollEvents();
 
         glUseProgram(prog);
-#if 1
-        glBegin(GL_TRIANGLES);
-        glColor3f(1.f, 0.f, 0.f);
-        glVertex3f(0.f, 0.f, 0.f);
-
-        glColor3f(0.f, 1.f, 0.f);
-        glVertex3f(1.f, 0.f, 0.f);
-
-        glColor3f(0.f, 0.f, 1.f);
-        glVertex3f(0.f, 1.f, 0.f);
-
-        glEnd();
-#endif
+        glRecti(-1,-1,1,1);
 
         glfwSwapBuffers(l_Window);
     }
