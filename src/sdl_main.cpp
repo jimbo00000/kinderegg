cacceb3257d49c121f9994ee37adbf8db1a31da3
jimbo00000 <james.susinno@gmail.com>
2015-03-08 20:46:20 -0400

    Add in basic audio init and callback to SDL main.

--- src/sdl_main.cpp
+++ src/sdl_main.cpp
@@ -70,6 +70,82 @@ void display()
 }
 
 
+//
+// Audio
+//
+
+// prototype for our audio callback
+void my_audio_callback(void *userdata, Uint8 *stream, int len);
+
+const int samps = 1024*8; // 65536 bytes appears to be max chunk size
+const int bufSz = samps*sizeof(Sint16); //*channels
+Uint8 *sound_buffer = NULL;
+Uint32 audio_pos = 0;
+int counter = 0;
+
+
+void play_audio()
+{
+    sound_buffer = new Uint8[bufSz];
+    audio_pos = 0;
+
+    static SDL_AudioSpec wav_spec;
+    wav_spec.freq = 44100;
+    wav_spec.format = AUDIO_S16LSB;
+    wav_spec.channels = 1;
+    wav_spec.samples = samps;
+    wav_spec.callback = my_audio_callback;
+    wav_spec.userdata = NULL;
+
+    // Open the audio device
+    if ( SDL_OpenAudio(&wav_spec, NULL) < 0 )
+    {
+        fprintf(stderr, "Couldn't open audio: %s\n", SDL_GetError());
+        exit(-1);
+    }
+    SDL_PauseAudio(0); // Start playing
+}
+
+int cb = 0;
+
+// audio callback function
+// here you have to copy the data of your audio buffer into the requesting audio buffer (stream)
+// you should only copy as much as the requested length (len)
+void my_audio_callback(void *userdata, Uint8 *stream, int len)
+{
+    printf("   callback:  %x  %d  counter:%d   ap: %d\n", stream, len, counter, audio_pos);
+
+    double pi = 3.1415;
+    double Hz = 440 * ((cb%2)+1);// + (counter/256);
+    double A = 32767;
+    double SR = 44100;
+    double F=2*pi*Hz/SR;
+
+    Sint16* stream16 = reinterpret_cast<Sint16*>(sound_buffer);
+    const int len16 = len / sizeof(Sint16);
+    for (int z = 0; z<len16; z++)
+    {
+        counter++;
+        stream16[z] = (Sint16) A*sin(F*(double)counter);
+    }
+
+    // Loop to beginning of buffer
+    if (audio_pos + len > bufSz)
+    {
+        audio_pos = 0;
+    }
+
+    SDL_memset(stream, 0, len);
+    const Uint8* apos = sound_buffer + audio_pos;
+    //SDL_memcpy (stream, apos, len);                  // simply copy from one buffer into the other
+    SDL_MixAudio(stream, apos, len, SDL_MIX_MAXVOLUME);// mix from one buffer into another
+
+    audio_pos += len;
+    ++cb;
+}
+
+
+
 int main(void)
 {
     ///@todo cmd line aargs
@@ -122,6 +198,8 @@ int main(void)
     g_toy.uloc_iResolution = glGetUniformLocation(g_toy.prog, "iResolution");
     g_toy.uloc_iGlobalTime = glGetUniformLocation(g_toy.prog, "iGlobalTime");
 
+    //play_audio();
+
     int quit = 0;
     while (quit == 0)
     {
@@ -132,6 +210,6 @@ int main(void)
 
     SDL_GL_DeleteContext(glContext);
     SDL_DestroyWindow(pWindow);
-
+    SDL_CloseAudio();
     SDL_Quit();
 }
