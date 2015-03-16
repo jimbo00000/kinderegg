dea113c550fbe6360828e7b3f124c17ac7be3512
jimbo00000 <james.susinno@gmail.com>
2015-03-15 20:21:59 -0400

    Add missing channel variable declarations.

--- tools/hardcode_shadertoy.py
+++ tools/hardcode_shadertoy.py
@@ -22,7 +22,11 @@ imageHeader = """
 #version 120
 uniform vec3 iResolution; // viewport resolution (in pixels)
 uniform float iGlobalTime; // shader playback time (in seconds)
-//uniform vec3      iChannelResolution[4]; // channel resolution (in pixels)
+uniform vec3      iChannelResolution[4]; // channel resolution (in pixels)
+uniform sampler2D iChannel0;
+uniform sampler2D iChannel1;
+uniform sampler2D iChannel2;
+uniform sampler2D iChannel3;
 uniform vec4      iMouse;                // mouse pixel coords. xy: current (if MLB down), zw: click
 uniform vec4      iDate;                 // (year, month, day, time in seconds)
 
