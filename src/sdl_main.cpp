509c87d484edddcc0f2ccb94700dc5c749c45559
jimbo00000 <james.susinno@gmail.com>
2015-03-20 23:09:45 -0400

    Trying to get texture settings right - Volcanic is still wrong.

--- src/sdl_main.cpp
+++ src/sdl_main.cpp
@@ -327,17 +327,18 @@ int main(void)
         std::vector<char> buffer(size);
         if (file.read(buffer.data(), size))
         {
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+            glTexParameteri(GL_TEXTURE_2D, GL_GENERATE_MIPMAP, GL_TRUE);
             glTexImage2D(GL_TEXTURE_2D,
                 0, mode,
                 w, h,
                 0, mode,
                 GL_UNSIGNED_BYTE,
                 &buffer[0]);
+            glGenerateMipmap(GL_TEXTURE_2D);
             r.texs[i] = t;
         }
     }
